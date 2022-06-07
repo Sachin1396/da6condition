9bf1b7962f0b4636d4484095d4394892f02792a1
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-07 09:39:40 +0530

    Repo: adding code to whileflip

--- whileflip.sh
+++ whileflip.sh
@@ -1,7 +1,14 @@
-echo "enter number"
-a=7
-while [ $a -gt 4 ]
+echo "flip coin"
+head=0
+tail=0
+while [[ $head -le 10 && $tail -le 10 ]]
 do
-	echo $a
-	((a--))
+	flip=$(($((RANDOM%10))%2))
+	if [[ $flip -eq 1 ]]
+	then
+		head=$(($head+1))
+	else
+		tail=$(($tail+1))
+	fi
 done
+echo "head :$head and tail :$tail" 
